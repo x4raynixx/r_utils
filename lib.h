6fb8e7e054dbd587cb4a6060a9a3388a7449cda4
Keyyq YT <ytkeyyq@gmail.com>
2025-07-29 12:42:05 +0200

    Lib Deafult 1.0.0

---
+++ lib.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+inline std::string makelower(std::string conv) {
+    std::transform(conv.begin(), conv.end(), conv.begin(),
+                   [](unsigned char c){ return std::tolower(c); });
+    return conv;
+}
+
+inline char makelower(char conv) {
+    return std::tolower(static_cast<unsigned char>(conv));
+}
